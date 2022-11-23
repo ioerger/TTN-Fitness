f3b86834a15c17f1d7223f3f18e8ec6c4a100612
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 08:21:51 +0000

    Adding tarfile member sanitization to extractall()

--- predict_with_STLM.py
+++ predict_with_STLM.py
@@ -19,7 +19,29 @@ python3 predict_with_STLM.py model.pickle trainTTN.csv testTTN.csv > predictions
 """
 
 with tarfile.open(sys.argv[1]+'.tar.gz', 'r') as t:
-    t.extractall('')
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(t, "")
 reg = sm.load(sys.argv[1])
 os.remove(sys.argv[1])
 
